3d6f48c686bbfe726c9e016488d0475ea7612fb9
SafaS1234 <157080937+SafaS1234@users.noreply.github.com>
2024-03-27 16:45:10 -0500

    fixed the median and added a mean function

--- README.md
+++ README.md
@@ -8,11 +8,3 @@ Run in VS Code.
 
 ### Running the Test Suite
 Run in VS Code.
-
-## Helpful Checklist
-
-This is just a helpful checklist that you can use to make sure that you project is ready for review. Feel free to delete this section when you are done with it.
-
-- [ ] Code is well-organized and easy to read
-- [ ] Code is tested via driver functions, assertions, or a combination of both (as appropriate)
-      
--- main.cpp
+++ main.cpp
@@ -1,8 +1,84 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
+vector <double> readFile(string name)
+{
+    vector <double> num;
+    double fileNum;
+    char comma;
+    ifstream f;
+
+    f.open(name);
+
+    if (f.is_open())
+    {
+        while(f >> fileNum)
+    {
+        num.push_back(fileNum);
+        f >> comma;
+    }
+    }
+    
+    else
+    {
+        cout << "Sorry, the file could not be opened." << endl;
+    }
+
+    f.close();
+
+    return num;
+}
+
+double median(vector<double> num)
+{
+    double median;
+    size_t size = num.size();
+
+    if (size % 2 != 0) 
+    {
+        median = num[size/2];
+    }
+
+    else
+    {
+        median = (num[size/2-1]+num[size/2]) / 2.0;
+    }
+
+    return median;
+
+}
+
+double mean(vector<double> num)
+{
+    double mean, total;
+    size_t size = num.size();
+
+    for (size_t i = 0; i < size; i++)
+    {
+        total += num[i];
+    }
+
+    mean = total/size;
+
+    return mean;
+
+}
+
 int main()
 {
-  cout << "Hello template!" << endl;
+    vector<double> num;
+    string name;
+
+    cout << "Enter the filename: ";
+    getline(cin, name);
+    
+    num = readFile(name);
+
+    cout << "The median of the data set is: " <<median(num)<< "." << endl;
+    cout << "The mean of the data set is: " << mean(num) << "." << endl;
+    //cout << "The mode of the data set is: " << mode(num) << "." << endl;
 }
+
